f38be8e0841adbfa5773b62ddb4d657ae6117447
한대승 <hoksi@forbiz.co.kr>
2019-12-05 11:30:19 +0900

    Template_ 추가

---
+++ application/views/Template_/_compile/index.tpl.php
@@ -0,0 +1,10 @@
+<?php /* Template_ 2.2.8 2019/12/04 18:26:41 C:\Bitnami\wampstack-7.2.22-0\apache2\htdocs\CodeIgniter3\application\views\Template_\_template\index.tpl 000000300 */ ?>
+<html>
+<head>
+<title><?php echo $TPL_VAR["title"]?></title>
+</head>
+<body>
+<?php echo $TPL_VAR["content"]?>
+
+</body>
+</html>
